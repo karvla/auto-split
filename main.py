3dc36aa149c3719c8f131c3e2041e9d67692ce2f
Arvid Larsson <arvid94@gmail.com>
2024-08-03 00:09:05 +0200

    feat: use sqllite instead of google sheets

--- main.py
+++ main.py
@@ -1,76 +1,147 @@
 from fasthtml.common import *
 from dotenv import load_dotenv
-from gcsa.event import Event
-from gcsa.google_calendar import GoogleCalendar
-from gcsa.calendar import CalendarListEntry
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from datetime import date
-import gspread
+from datetime import datetime
 import os
 
 load_dotenv()
 
+app, rt = fast_app(live=True)
+db = database("data/carpool.db")
 
-CALENDAR_URL = os.getenv("CALENDAR_URL")
-CALENDAR_ID = os.getenv("CALENDAR_ID")
-SHEET_ID = os.getenv("SHEET_ID")
+users, bookings = db.t.users, db.t.bookings
+if users not in db.t:
+    users.create(name=str, pk="name")
+    for user in os.getenv("USERS").split(","):
+        users.insert(name=user)
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-credentials = service_account.Credentials.from_service_account_file(
-    'service_account.json', scopes=SCOPES)
-
-app,rt = fast_app()
-
-gc = gspread.service_account(filename="service_account.json")
-
-def get_people() -> List[str]: 
-    return gc.open_by_key(SHEET_ID).get_worksheet(3).col_values(1)[1:]
-
-@rt('/')
-def get(): return Div(
-    add_form(),
-    calendar() 
-) 
-
-def add_form():
-    return Form(
-        Group(
-            Select(
-                *[Option(p) for p in get_people()],
-                name="booker"
-            ),
-            Input(type="date", name="from"), 
-            Input(type="date", name="from"), 
-            Input(type="text", name="note", placeholder="note"),
-            Button("Add")),
-                hx_post="/")
-
-def calendar(): return Iframe(src=f'https://calendar.google.com/calendar/embed?src={get_calendar_id()}&ctz=Europe%2FStockholm', width="100%", height="600")
-
-def get_calendar_id():
-    return GoogleCalendar(credentials=credentials).get_calendar().calendar_id
+    bookings.create(
+        id=int, note=str, date_from=datetime, date_to=datetime, user=str, pk="id"
+    )
+Booking, User = bookings.dataclass(), users.dataclass()
 
 
-def update_calendar():
-    gc = GoogleCalendar(credentials=credentials)
-    events = list(gc.get_events())
+@rt("/")
+def get():
+    return Div(
+        H1("Car pool"),
+        Button("New booking", hx_get="/bookings/add", hx_target="body"),
+        bookings_table(),
+        #    calendar()
+    )
 
-    start = date(2024,8,1)
-    end = date(2024,8,2)
-    event = Event('Vacation',
-                  visibility="public",
-              start=start,
-              end=end)
-    gc.add_event(event)
-    print(events)
-    for event in gc.get_events():
-        print(event)
 
-    
+@rt("/bookings/add")
+def get(error_msg=""):
+    return Div(
+        Form(
+            booking_form(Booking(), error_msg=""),
+            hx_target="this",
+            hx_swap="outerHTML",
+            hx_post="/bookings/add",
+            hx_push_url="/",
+        ),
+        Div(error_msg),
+    )
 
-update_calendar()
+
+@rt("/bookings/edit/{id}")
+def get(id: int, error_msg=""):
+    return Div(
+        Form(
+            booking_form(bookings[id], error_msg),
+            hx_target="this",
+            hx_swap="outerHTML",
+            hx_post=f"/bookings/edit/{id}",
+            hx_push_url="/",
+        ),
+        Div(error_msg),
+    )
+
+
+@app.post("/bookings/add")
+def add_new_booking(booking: Booking):
+    if not booking.date_from or not booking.date_to:
+        return booking_form(booking, error_msg="Pls add from/to date")
+    bookings.insert(booking)
+    return RedirectResponse("/", status_code=303)
+
+
+@app.post("/bookings/edit/{id}")
+def edit_booking(booking: Booking, id: int):
+    if not booking.date_from or not booking.date_to:
+        return booking_form(booking, error_msg="Pls add from/to date")
+    bookings.update(booking)
+    return RedirectResponse("/", status_code=303)
+
+
+@app.delete("/bookings/{id}")
+def delete_booking(id: int):
+    bookings.delete(id)
+    return RedirectResponse("/", status_code=303)
+
+
+def booking_form(booking: Booking, error_msg=""):
+    return Group(
+        Select(*[Option(u.name) for u in users()], name="user"),
+        Input(type="text", name="id", value=booking.id, style="display:none"),
+        Input(type="date", name="date_from", value=booking.date_from),
+        Input(type="date", name="date_to", value=booking.date_to),
+        Input(type="text", name="note", placeholder="note", value=booking.note),
+        Button("Update" if booking.id else "Add"),
+        Div(error_msg),
+        style="flex-direction: column",
+    )
+
+
+def bookings_table():
+    return Table(
+        Tr(Th("Note"), Th("From"), Th("To"), Th("User"), Th(), Th()),
+        *[
+            Tr(
+                Td(b.note),
+                Td(b.date_from),
+                Td(b.date_to),
+                Td(b.user),
+                Td(A("Edit", href=f"/bookings/edit/{b.id}")),
+                Td(
+                    Button(
+                        "Delete",
+                        hx_delete=f"/bookings/{b.id}",
+                        hx_confirm="Are you sure you want to delete this booking?",
+                        hx_swap="delete",
+                        hx_target="closest tr",
+                    )
+                ),
+            )
+            for b in bookings()
+        ],
+    )
+
+
+# def calendar(): return Iframe(src=f'https://calendar.google.com/calendar/embed?src={get_calendar_id()}&ctz=Europe%2FStockholm', width="100%", height="600")
+
+# def get_calendar_id():
+#    return GoogleCalendar(credentials=credentials).get_calendar().calendar_id
+
+
+# def update_calendar():
+#    gc = GoogleCalendar(credentials=credentials)
+#    events = list(gc.get_events())
+#
+#    start = date(2024,8,1)
+#    end = date(2024,8,2)
+#    event = Event('Vacation',
+#                  visibility="public",
+#              start=start,
+#              end=end)
+#    gc.add_event(event)
+#    print(events)
+#    for event in gc.get_events():
+#        print(event)
+#
+
+
+# update_calendar()
 
 
 serve()
