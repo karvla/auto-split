46bff525c8a586a0495c4dcf934d497891f65da9
Arvid Larsson <arvid94@gmail.com>
2024-09-28 12:55:03 +0200

    feat: hosts multiple calendars

--- car_pool/bookings_calendar.py
+++ car_pool/bookings_calendar.py
@@ -1,5 +1,4 @@
-from app import app, calendar_path
-from bookings import Booking
+from app import app
 from config import BASE_URL
 from db.init_db import load_database
 from fasthtml.common import Response
@@ -7,12 +6,37 @@ from ics import Calendar, Event
 
 db = load_database()
 bookings = db.t.bookings
+cars = db.t.cars
+Car = cars.dataclass()
+Booking = bookings.dataclass()
 
 
-@app.get(calendar_path)
-def ics_content():
+def calendar_secret(sess=None):
+    if sess is None:
+        return None
+    secret = db.query(
+        """
+        select calendar_secret
+        from cars
+        where cars.id = (select car_id
+                        from users
+                        where users.name = ?
+                        limit 1)
+                 """,
+        [sess["auth"]],
+    )
+    return next(iter(secret))["calendar_secret"]
+
+
+@app.get("/calendar/{calendar_path}")
+def ics_content(calendar_path: str):
+    calendar_secret, _ = calendar_path.split(".")
+    c = cars(where="calendar_secret = ?", where_args=[calendar_secret], limit=1)
+    if len(c) == 0:
+        return Response(status_code=404)
+
     return Response(
-        to_ics_content(bookings()),
+        to_ics_content(bookings(where="car_id = ?", where_args=[c[0].id])),
         headers={"Content-Disposition": "attachment", "Content-Type": "text/calendar"},
     )
 
